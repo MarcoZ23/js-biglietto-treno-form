67a2d7c6f7a1ee1f9b94eb37ddb79e09d5167976
MarcoZ23 <zollomarco23@gmail.com>
2026-03-17 23:06:45 +0100

    complete milestone 1

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+/*
+il prezzo del biglietto è definito in base ai km (0.21 € al km),
+va applicato uno sconto del 20% per i minorenni,
+va applicato uno sconto del 40% per gli over 65. 
+*/
+
+
+const price = 0.21
+const age = document.getElementById("age")
+const km = document.getElementById("km") 
+const formEl = document.querySelector("form")
+
+
+    formEl.addEventListener('submit', function(e){
+        e.preventDefault()
+
+    let finalPrice = price * km.value 
+
+    if (age.value < 18) {
+        finalPrice = finalPrice * 0.80} 
+    else if (age.value >= 65) {
+        finalPrice = finalPrice * 0.60} 
+
+console.log(finalPrice);
+
+})
+
+
+
 
